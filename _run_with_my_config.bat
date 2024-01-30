3fbd1bb3ff27c0bcebea7b6e51f5bfb1ca514b66
iksi4prs <iksi4prs@gmail.com>
2024-01-30 09:20:23 +0200

    fixed path

--- _run_with_my_config.bat
+++ _run_with_my_config.bat
@@ -1,4 +1,3 @@
-REM Alacritty-v0.11.0-portable -vv --config-file C:\MyProjects\\DotFiles\Alacritty\alacritty.yml
-REM Alacritty.exe -vv --config-file C:\MyProjects\\DotFiles\Alacritty\alacritty.toml
-set tool=C:\MyProjects\Alacritty\alacritty\target\release\Alacritty.exe
+
+set tool=C:\MyProjects\Alacritty\Fork\alacritty\target\release\Alacritty.exe
 %tool% -vvvvvv --print-events --config-file C:\MyProjects\DotFiles\Alacritty\alacritty.toml
