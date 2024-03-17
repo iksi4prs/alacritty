258132cb0e2a35fca6a0492d0d4fb1b3ddb07ae3
iksi4prs <iksi4prs@gmail.com>
2024-03-17 13:04:43 +0200

    updated bat file

--- _run_with_my_config.bat
+++ _run_with_my_config.bat
@@ -1,3 +1,5 @@
 
 set tool=C:\MyProjects\Alacritty\Fork\alacritty\target\release\Alacritty.exe
-%tool% -vvvvvv --print-events --config-file C:\MyProjects\DotFiles\Alacritty\alacritty.toml
+REM --print-event also print all windows events for mouse move, etc (see "winit event" in code, and self.config.debug.print_events  )
+REM %tool% -vvvvvv --print-events --config-file C:\MyProjects\DotFiles\Alacritty\alacritty.toml
+%tool% -vvvvvv --config-file C:\MyProjects\DotFiles\Alacritty\alacritty.toml
